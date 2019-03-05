2681aa4ddbef01e1417c0b5c8d8131372008e6c9
RuneMadsen <webudvikler2020@gmail.com>
2019-03-05 17:34:19 +0100

    Opgave 1 med rettelse

--- hjemmeopgave/script/dfi-opg1.js
+++ hjemmeopgave/script/dfi-opg1.js
@@ -1,42 +1,42 @@
 //Globale variabler
-var wsurl;
+var dfiurl;
 
 
 
 //det der skal ske når siden loader
 window.onload = function(){
-
+    
 
 
     //url/adresse til webservicen i variabel
-    wsurl = "https://api.dfi.dk/v1/film/74245";
+    dfiurl = "https://api.dfi.dk/v1/film/74245";
 
-
+    kaldWerbservice();
 };
 
 //function til at kalde webservicen
 function kaldWerbservice(){
 
-        fetch(wsurl, {
-            method: 'get',
-            headers: new Headers({
-                //medsend brugernavn og password til DFI
-                Authorization: "Basic " + btoa('F005936:JRbT1fWVMH0bm3n')
-            })
-        }).then(function(response){
-
-
-            return response.json();
-        }).then(function(jsonsvar){
-
-
-            console.log(jsonsvar);
-        }).catch(function(error){
-
-
-            console.log("Der er sket en fejl!!!!");
-            alert("FEJL!!!");
+    fetch(dfiurl, {
+        method: 'get',
+        headers: new Headers({
+            Authorization: "basic " + btoa('F005936:JRbTlfWVMH0bm3n')
         })
+    }).then(function(response){
+        
+
+        return response.json();
+
+    }).then(function(jsonsvar){
+
+        console.log(jsonsvar);
+    }).catch(function(error){
+
+        console.log("Der er sket en fejl!!");
+        alert("FEJL!!!");
+    })
+
+
 
 
 
